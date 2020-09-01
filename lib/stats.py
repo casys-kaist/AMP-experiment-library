fb319ed75ad3b94af8aba123df77277defc04fe3
Taekyung Heo <tkheo@casys.kaist.ac.kr>
2020-09-01 21:36:45 +0900

    Update lib/stats.py

--- lib/stats.py
+++ lib/stats.py
@@ -27,3 +27,27 @@ def record_stats_per_cgroup(result_dir, signature, cgroup_li, stat_name):
     for idx, cgroup in enumerate(cgroup_li):
         with open("%s/%s_%d.%s.txt" % (result_dir, signature, idx, stat_name), "a") as f:
             f.write(cgroup.get_str(stat_name))
+
+speccpu2017_abbr_dict = {
+            "605.mcf_s": "mcf",
+            "631.deepsjeng_s": "deepsjeng",
+            "603.bwaves_s": "bwaves",
+            "607.cactuBSSN_s": "cactus",
+            "619.lbm_s": "lbm",
+            "628.pop2_s": "pop2",
+        }
+
+cloudsuite_abbr_dict = {
+            "graph-analytics": "graph-analytics",
+            "in-memory-analytics": "in-mem-analytics"
+        }
+
+others_abbr_dict = {
+            "graph500:bfs:23:13": "graph500",
+        }
+
+def abbreviate_workload_name(workload_name):
+    for abbr_dict in [speccpu2017_abbr_dict, cloudsuite_abbr_dict, others_abbr_dict]:
+        if workload_name in abbr_dict.keys():
+            return abbr_dict[workload_name]
+    return workload_name
