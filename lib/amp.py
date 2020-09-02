99b162d59ff0305fb70d7e72057f091b3ea9e0fa
Taekyung Heo <tkheo@casys.kaist.ac.kr>
2020-09-02 13:24:51 +0900

    Add lib/amp.py

---
+++ lib/amp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+   amp.py
+
+    Created on: Apr. 11, 2020
+        Author: Taekyung Heo <tkheo@casys.kaist.ac.kr>
+"""
+
+MIG_POLICY_NOP = 0
+MIG_POLICY_RANDOM = 1
+MIG_POLICY_MODIFIED_LRU_LISTS = 2
+MIG_POLICY_LRU = 3
+MIG_POLICY_LFU = 4
